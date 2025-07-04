331204e97f23a65a14be78bb2c2b7d5a75f88c57
2GreekDevs <2greekdevs24@gmail.com>
2025-07-04 14:29:51 +0300

    Add basename to React Router for GitHub Pages

--- 2greekdevs/src/App.tsx
+++ 2greekdevs/src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/2GreekDevs-Portofolio">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
